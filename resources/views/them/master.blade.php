f28f1260a95d5a17d4b9b9c3c1eb612df70298d5
Elsayed Elbeshry <elsayedelbeshry119@gmail.com>
2022-03-18 00:22:41 +0200

    Them

---
+++ resources/views/them/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('them.includes.head')
+
+<body class="home-page home-01 ">
+
+    @include('them.includes.header')
+
+    <main id="main">
+     @yield('content')
+
+    </main>
+
+    @include('them.includes.footer')
+    @include('them.includes.js')
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('them.home');
 });
 
 Route::get('/dashboard', function () {
