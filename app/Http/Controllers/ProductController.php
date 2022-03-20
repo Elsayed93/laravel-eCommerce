b7fb653bcbfba09800264d55ea9613bfddbf6f0b
Elsayed Elbeshry <elsayedelbeshry119@gmail.com>
2022-03-20 22:40:08 +0200

    Popular products and related products in product details page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,10 +12,11 @@ class ProductController extends Controller
     public function show($slug)
     {
         $product = Product::where('slug', $slug)->first();
-
+        $popularProducts = Product::inRandomOrder()->limit(4)->get();
+        $relatedProducts = Product::where('category_id', $product->category_id)->inRandomOrder()->get();
 
         if ($product) {
-            return view('products.show', compact('product'));
+            return view('products.show', compact('product', 'popularProducts', 'relatedProducts'));
         } else {
             return abort(404);
         }
